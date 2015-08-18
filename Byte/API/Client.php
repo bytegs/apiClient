4f54ce21984e38e64c996287ab798a1fb0eed160
Soeren <soeren@siegismund-poschmann.de>
2015-08-18 17:00:03 +0200

    Stuff

---
+++ Byte/API/Client.php
@@ -0,0 +1,33 @@
+<?php
+namespace Byte\Auth;
+class User
+{
+  public function __construct($apiKey, $url = NULL)
+  {
+    $this->url = $url;
+    $this->apiKey = $apiKey;
+    $this->curl = new \Curl;
+    $this->curl->headers["X-Auth-Token"] = $apiKey;
+  }
+  public function get($path)
+  {
+    $res = $this->curl->get($this->url.$path);
+    $ret = json_decode($res->body, true);
+    if($ret == NULL)
+    {
+      throw new \Exception("Return no JSON");
+    }
+    return $ret;
+  }
+  public function post($path, $data = [])
+  {
+    $res = $this->curl->post($this->url.$path, $data);
+    $ret = json_decode($res->body, true);
+    if($ret == NULL)
+    {
+      throw new \Exception("Return no JSON");
+    }
+    return $ret;
+  }
+
+}
