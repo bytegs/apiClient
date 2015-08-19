561ae9d8a90caae14fe971cb798afab09bf1774d
Soeren <soeren@siegismund-poschmann.de>
2015-08-19 14:52:26 +0200

    Fix Bug

--- Byte/API/Exception.php
+++ Byte/API/Exception.php
@@ -5,7 +5,7 @@ class Exception
     protected $result;
     public function __construct($message="", $code=0 , Exception $previous=NULL, $result)
     {
-        $this->_field = result;
+        $this->$result = $result;
         parent::__construct($message, $code, $previous);
     }
     public function getResult()
